96c42a5706f5546081ffd54a506a76f46721a3c4
srikumar2050 <srikumar.k@gmail.com>
2026-06-10 22:47:31 +0530

    Add files via upload

--- tests/test_execution_mode.py
+++ tests/test_execution_mode.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
-from hugiml import HUGIMLClassifierNative
 import hugiml.classifier as classifier_module
+from hugiml import HUGIMLClassifierNative
 
 
 def _binary_data(n: int = 120, p: int = 4):
